2fcae06d29e946c0fe7ad1f860fde23f3063d4f8
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2020-03-26 19:03:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -32,12 +32,11 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
 end
 
 
-# def add_supply_to_memorial_day(holiday_hash, supply)
+def add_supply_to_memorial_day(holiday_hash, supply)
   
   holiday_hash[:spring][:memorial_day][1].push(supply)
   
- 
-# end
+end
 
 def add_supply_to_memorial_day(holiday_hash, supply)
  
